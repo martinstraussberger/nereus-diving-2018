ccbdc6841b56fe17d6cd8d6e9e2ff547651d40d0
Martin Straußberger <>
2023-01-24 17:17:44 +0100

    changed pages propr from /docs to /roots for build pipeline. Prevent failing pipes

--- public/js/redirect.js
+++ public/js/redirect.js
@@ -2,25 +2,25 @@
 
 // redirect to ScubaShack
 document.getElementById('redirect-scubashack').onclick = function redirectScubaShack() {
-	window.location.href = '../pages/partners/ScubaShack.html';
+	window.location.href = './pages/partners/ScubaShack.html';
 };
 
 // redirect to Fernsteinsee
 document.getElementById('redirect-fernsteinsee').onclick = function redirectFernSteinSee() {
-	window.location.href = '../pages/partners/Fernsteinsee.html';
+	window.location.href = './pages/partners/Fernsteinsee.html';
 };
 
 // redirect to Bunaken
 document.getElementById('redirect-bunaken').onclick = function redirectBunaken() {
-	window.location.href = '../pages/partners/Bunaken.html';
+	window.location.href = './pages/partners/Bunaken.html';
 };
 
 // redirect to Calypso
 document.getElementById('redirect-calypso').onclick = function redirectCalypso() {
-	window.location.href = '../pages/partners/CalypsoMalta.html';
+	window.location.href = './pages/partners/CalypsoMalta.html';
 };
 
 // redirect to Atlantis
 document.getElementById('redirect-atlantis').onclick = function redirectAtlantis() {
-	window.location.href = '../pages/partners/AtlantisMalta.html';
+	window.location.href = './pages/partners/AtlantisMalta.html';
 };
