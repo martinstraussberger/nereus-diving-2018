eafe70e95a043074a888676c27a55ca28fa48b0a
Martin Straußberger <>
2023-01-25 10:58:29 +0100

    bug fix search.js url redirect

--- public/js/search.js
+++ public/js/search.js
@@ -19,7 +19,7 @@ import { searchIndex } from './autocomplete-list-objects.js';
       '">' +
       '<div class="search-filter"' +
       '">' +
-      '<a href="' +
+      '<a href="' + '/pages/partners/' +
       partner.url +
       '">' +
       '<h4 class="partner-h4">' +
