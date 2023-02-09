bfdc5413b9bb7550455a5754885f7d4e853cc694
Martin Straußberger <>
2023-02-09 14:32:07 +0100

    redirect js

--- public/js/redirect.js
+++ public/js/redirect.js
@@ -2,25 +2,25 @@
 
 // redirect to ScubaShack
 document.getElementById('redirect-scubashack').onclick = function redirectScubaShack() {
-	window.location.href = 'pages/partners/Scubashack.html';
+	window.location.href = './pages/partners/Scubashack.html';
 };
 
 // redirect to Fernsteinsee
 document.getElementById('redirect-fernsteinsee').onclick = function redirectFernSteinSee() {
-	window.location.href = 'pages/partners/Fernsteinsee.html';
+	window.location.href = './pages/partners/Fernsteinsee.html';
 };
 
 // redirect to Bunaken
 document.getElementById('redirect-bunaken').onclick = function redirectBunaken() {
-	window.location.href = 'pages/partners/Bunaken.html';
+	window.location.href = './pages/partners/Bunaken.html';
 };
 
 // redirect to Calypso
 document.getElementById('redirect-calypso').onclick = function redirectCalypso() {
-	window.location.href = 'pages/partners/Calypsomalta.html';
+	window.location.href = './pages/partners/Calypsomalta.html';
 };
 
 // redirect to Atlantis
 document.getElementById('redirect-atlantis').onclick = function redirectAtlantis() {
-	window.location.href = 'pages/partners/Atlantismalta.html';
+	window.location.href = './pages/partners/Atlantismalta.html';
 };
